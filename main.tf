cad671920046316129ac8b67192fd9b13759999e
FloydLeHippie <104831656+FloydLeHippie@users.noreply.github.com>
2023-06-29 09:15:15 +0200

    Add files via upload

--- main.tf
+++ main.tf
@@ -11,6 +11,8 @@ terraform {
 provider "proxmox" {
   pm_api_url = "https://192.168.1.1:8006/api2/json"
   pm_tls_insecure = true
+  pm_user = var.pm_user
+  pm_password = var.pm_password
 }
 
 resource "proxmox_vm_qemu" "test_vm" {
@@ -26,27 +28,26 @@ resource "proxmox_vm_qemu" "test_vm" {
   memory = 4096
   scsihw = "virtio-scsi-pci"
   bootdisk = "scsi0"
-
-  cdrom {
-    volume_id = "local:iso/lubuntu-23.04-desktop-amd64.iso"
-    iso      = "local:iso/lubuntu-23.04-desktop-amd64.iso"
-  }
+  iso      = "local:iso/lubuntu-23.04-desktop-amd64.iso"
 
   disk {
-    slot = 0
-    # set disk size here. leave it small for testing because expanding the disk takes time.
+    # slot = 0
     size = "40G"
     type = "scsi"
     storage = "local-lvm"
-    iothread = 1
+    # iothread = 1
   }
   
-  # if you want two NICs, just copy this whole network section and duplicate it
   network {
     model = "virtio"
     bridge = "vmbr0"
   }
 
-  ipconfig0 = "ip=192.168.1.75/24,gw=192.168.1.254"
+  # cdrom {
+  #   volume_id = "local:iso/lubuntu-23.04-desktop-amd64.iso"
+  #   iso      = "local:iso/lubuntu-23.04-desktop-amd64.iso"
+  # }
+
+  # ipconfig0 = "ip=192.168.1.75/24,gw=192.168.1.254"
   
 }
